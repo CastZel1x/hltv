7df1a618421702b384fdf802ba860c2d3ee5aaba
Denis <casterzel1x@gmail.com>
2023-04-01 13:52:41 +0300

    created

---
+++ __tests__/getEvent.test.ts
@@ -0,0 +1,19 @@
+import HLTV from '../src/'
+import { sleep } from '../src/utils'
+
+const FULL = 5785
+const NO_INFO = 355
+const WITH_QUALIFIED_FOR = 4356
+const WITH_STRANGE_PRIZE = 2870
+
+test('getEvent', async () => {
+  await sleep(3000)
+  expect(await HLTV.getEvent({ id: FULL })).toMatchSnapshot()
+  await sleep(3000)
+  expect(await HLTV.getEvent({ id: NO_INFO })).toMatchSnapshot()
+  await sleep(3000)
+  expect(await HLTV.getEvent({ id: WITH_QUALIFIED_FOR })).toMatchSnapshot()
+  await sleep(3000)
+  expect(await HLTV.getEvent({ id: WITH_STRANGE_PRIZE })).toMatchSnapshot()
+  await sleep(3000)
+}, 30000)
